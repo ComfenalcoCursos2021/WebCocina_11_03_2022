3fd0eaa251ff8d8b5f38db258e59c8cc436a1212
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-03-14 19:58:01 -0500

    Maquetacion Menu JS, module

--- js/main.js
+++ js/main.js
@@ -3,11 +3,23 @@ addEventListener("DOMContentLoaded", async()=>{
     //Peticion al archivo PHP
     let peticion = await fetch("backend/php/api.php");
     let obj = await peticion.json();
+    let fragmen = (container, lista)=>{
+        let frag = new DocumentFragment();
+        for (let i = 0; i < lista.length; i++) {
+            let cont = document.createElement(container);
+            let a = document.createElement('A');
+            Object.assign(a, lista[i]);
+            cont.insertAdjacentElement("beforeend", a);
+            frag.append(cont);
+        }
+        return frag;
+    }
     // selecion del DOM 'maquetacion web'
     let content = document.querySelector(".content");
+    let header = document.querySelector("header");
     let titulo = document.querySelector(".content h2");
     let parrafo = document.querySelector(".content p");
-
+    let menu = document.querySelector("header ul");
     console.log(obj);
 
     // Implemetacion del modulo
@@ -20,5 +32,13 @@ addEventListener("DOMContentLoaded", async()=>{
     a.classList = "btn";
     content.insertAdjacentElement("beforeend", a);
 
+    let img = document.createElement('IMG');
+    img.src = obj.menu.img;
+    img.width = "120";
+    header.insertAdjacentElement("beforeend",img);
+
+    menu.append(fragmen("LI",fn.lista(obj.menu,"href","innerText")));
+
+    
 })
 
--- js/modules/function.js
+++ js/modules/function.js
@@ -5,7 +5,18 @@ const descriptcion = (obj)=>{
     });
     return data;
 }
-
+const lista = (obj,atrr,val)=>{
+    let data = [];
+    Object.values(obj.enlace).forEach(element => {
+        let obj = [];
+        let value = Object.values(element);
+        obj[`${val}`] = value[0];
+        obj[`${atrr}`] = value[1];
+        data.push({...obj});
+    });
+    return data;
+}
 export const fn = {
-    descriptcion
+    descriptcion,
+    lista
 };
